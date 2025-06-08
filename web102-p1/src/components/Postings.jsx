891c6cdc386f33600bdf630c76a64a8909003791
ReahKy23 <Kylah.kerr23@gmail.com>
2025-06-07 22:33:02 -0400

    Postings Component created

---
+++ web102-p1/src/components/Postings.jsx
@@ -0,0 +1,51 @@
+import React from "react"
+
+const Postings = () => {
+    return (
+
+        <div className="post-container">
+            <div className="post">
+                <div className="post-type">Post Type</div>
+                <img src="../assets/movies/sinners.jpg" />
+                <div>Name of type</div>
+                <div className="read-more">Read More Button</div>
+            </div>
+            <div className="post">
+                <div className="post-type">Post Type</div>
+                <div>Post Img </div>
+                <div>Name of type</div>
+                <div className="read-more">Read More Button</div>
+            </div>
+            <div className="post">
+                <div className="post-type">Post Type</div>
+                <div>Post Img </div>
+                <div>Name of type</div>
+                <div className="read-more">Read More Button</div>
+            </div>
+            <div className="post">
+                <div className="post-type">Post Type</div>
+                <div>Post Img </div>
+                <div>Name of type</div>
+                <div className="read-more">Read More Button</div>
+            </div>
+            <div className="post">
+                <div className="post-type">Post Type</div>
+                <div>Post Img </div>
+                <div>Name of type</div>
+                <div className="read-more">Read More Button</div>
+            </div>
+            <div className="post">
+                <div className="post-type">Post Type</div>
+                <div>Post Img </div>
+                <div>Name of type</div>
+                <div className="read-more">Read More Button</div>
+            </div>
+
+        </div>
+
+
+
+    )
+}
+
+export default Postings
