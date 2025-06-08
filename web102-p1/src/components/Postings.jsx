3115fead79cce335ea98b46c3edf5aaf380ddc56
ReahKy23 <Kylah.kerr23@gmail.com>
2025-06-07 22:49:14 -0400

    Img Component created

--- web102-p1/src/components/Postings.jsx
+++ web102-p1/src/components/Postings.jsx
@@ -1,42 +1,67 @@
 import React from "react"
+import Images from './Images'
 
 const Postings = () => {
     return (
 
         <div className="post-container">
             <div className="post">
-                <div className="post-type">Post Type</div>
-                <img src="../assets/movies/sinners.jpg" />
+                <div className="post-type">Movie</div>
+                <Images
+                    src="../src/assets/movies/sinners.jpg"
+                    alt="Sinners poster"
+                    className="post-img"
+                />
                 <div>Name of type</div>
                 <div className="read-more">Read More Button</div>
             </div>
             <div className="post">
                 <div className="post-type">Post Type</div>
-                <div>Post Img </div>
+                <Images
+                    src="../src/assets/movies/sinners.jpg"
+                    alt="Sinners poster"
+                    className="post-img"
+                />
                 <div>Name of type</div>
                 <div className="read-more">Read More Button</div>
             </div>
             <div className="post">
                 <div className="post-type">Post Type</div>
-                <div>Post Img </div>
+                <Images
+                    src="../src/assets/movies/sinners.jpg"
+                    alt="Sinners poster"
+                    className="post-img"
+                />
                 <div>Name of type</div>
                 <div className="read-more">Read More Button</div>
             </div>
             <div className="post">
                 <div className="post-type">Post Type</div>
-                <div>Post Img </div>
+                <Images
+                    src="../src/assets/movies/sinners.jpg"
+                    alt="Sinners poster"
+                    className="post-img"
+                />
                 <div>Name of type</div>
                 <div className="read-more">Read More Button</div>
             </div>
             <div className="post">
                 <div className="post-type">Post Type</div>
-                <div>Post Img </div>
+                <Images
+                    src="../src/assets/movies/sinners.jpg"
+                    alt="Sinners poster"
+                    className="post-img"
+                />
                 <div>Name of type</div>
                 <div className="read-more">Read More Button</div>
             </div>
             <div className="post">
                 <div className="post-type">Post Type</div>
-                <div>Post Img </div>
+                <Images
+                    src="../src/assets/movies/sinners.jpg"
+                    alt="Sinners poster"
+                    className="post-img"
+                />
                 <div>Name of type</div>
                 <div className="read-more">Read More Button</div>
             </div>
